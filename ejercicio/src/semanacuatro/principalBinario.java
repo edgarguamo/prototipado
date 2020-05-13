fb04e78993d8e1f07f353ec4fd0a67c87f8896b6
edgarguamo@gmail.com <edgarguamo@gmail.com>
2020-05-12 23:48:46 -0500

    Arreglo segundo intento

---
+++ ejercicio/src/semanacuatro/principalBinario.java
@@ -0,0 +1,15 @@
+package semanacuatro;
+
+import java.util.stream.IntStream;
+
+public class principalBinario {
+    public static void main(String[] args) {
+        int[] values = IntStream.range(0,10_000_000).toArray();
+        int index=0;
+        for (var j : values){
+            if(j==80) index= j;
+        }
+        System.out.println("Valor encontrado en ka posición "+index);
+    }
+
+}
