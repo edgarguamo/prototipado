6fd2e6eeb53d2b386b2fecfd90a4037650281df6
edgarguamo@gmail.com <edgarguamo@gmail.com>
2020-05-12 23:12:50 -0500

    Primera modificación

--- ejercicio/src/semanaCuatro/principal_binario.java
+++ ejercicio/src/semanaCuatro/principal_binario.java
@@ -1,22 +1,19 @@
-package semana4;
+package semanaCuatro;
 
 import java.util.stream.IntStream;
 
-public class principal_binario_80 {
+public class principal_binario {
     public static void main(String[] args) {
         int[] values = IntStream.range(0,10_000_000).toArray();
-       principal_binario_80.calculo(values);
+       principal_binario.calculo(values);
     }
     public static void calculo(int [] values){
         for (var i = 0; i<10_000_000;i++){
             int index = -1;
             for (var j=0;j<values.length;j++){
-                if (values[j]==80){
-                    index=j;
-                }
+                index+=(j==80) ? 1:0;
             }
             System.out.printf("valor posición j = %d",index);
-            break;
         }
     }
 }
